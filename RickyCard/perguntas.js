bafa5d122b34b6ac6e82a13c483d89e798e99d9e
Ricky080 <00001131863355sp@al.educacao.sp.gov.br>
2024-11-29 00:51:20 -0300

    Add files via upload

---
+++ RickyCard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'O que é CelsoPiva?',
+    'Celso Piva é uma escola pública.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Escola',
+    'O que é um Aluno PEI?',
+    'É um estudante que possui um ensino Integral.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
